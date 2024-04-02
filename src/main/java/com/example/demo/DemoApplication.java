1163b77904e6f12258583696a34348f443a7c264
veraakarthikbharadwaj <kbharadwaj@veracode.com>
2024-04-03 00:38:37 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
  
 public class DemoApplication {
-    public void Example1(String arg) throws IOException {    
+    public void example(String arg) throws IOException {    
         foo(arg);
     }
      
     public void foo(String arg) throws IOException {
         Runtime.getRuntime().exec(arg);
-    }           
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Inside Main function");
+    }
 }
