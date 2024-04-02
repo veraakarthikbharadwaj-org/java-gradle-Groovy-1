fc9e7e8dadc0025c0aa69d292229c4086ccb41e7
veraakarthikbharadwaj <kbharadwaj@veracode.com>
2024-04-03 00:29:55 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
  
-public class Example1 {
-    public Example1(String arg) throws IOException {    
+public class DemoApplication {
+    public void Example1(String arg) throws IOException {    
         foo(arg);
     }
      
